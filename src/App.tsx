f754a673d6be26ca90d3d599cf94e1c19485c92b
Bassem Ghazouani <bassem.ghazouani@hotmail.com>
2024-09-18 07:23:52 +0200

    INITIAL COMMIT - Project setup and configuration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    
+    return (
+        <main className="max-w-7xl mx-auto">
+            <h1 className="text-2xl text-white underline">Hello react !</h1>
+        </main>
+    ) 
+}
+
+export default App;
