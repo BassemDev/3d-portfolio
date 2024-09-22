9b933bbdb1ccf68961f34ff38f48f27cc017435b
Bassem Ghazouani <bassem.ghazouani@hotmail.com>
2024-09-22 17:15:05 +0200

    feat: introduce the navigation items which present the different pages that will be included in the portfolio and define the type related to the navigation tab.

---
+++ src/constants/navbar.ts
@@ -0,0 +1,29 @@
+import { NavLink } from "../types";
+
+export const NAV_ITEM_LABELS: NavLink[] = [
+    {
+        id: 1,
+        label: "Home",
+        href: "#Home"
+    },
+    {
+        id: 2,
+        label: "About",
+        href: "#About"
+    },
+    {
+        id: 3,
+        label: "Skills",
+        href: "#Skills"
+    },
+    {
+        id: 4,
+        label: "Projects",
+        href: "#Projects"
+    },
+    {
+        id: 5,
+        label: "Contact",
+        href: "#Contact"
+    }
+];
